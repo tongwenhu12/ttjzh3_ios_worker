630ef114c75c28c2e1142fca88de10b74cfe1fd3
Wenhu Tong <tongwenhu12@yahoo.com>
2023-02-11 14:52:45 -0500

    Initial Commit

---
+++ ttjzh3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ttjzh3
+//
+//  Created by WENHU TONG on 2023-02-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
